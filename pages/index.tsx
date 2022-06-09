82f0a53d01a03ec4d9c95024c9461342e8f019f8
Vladut Ilie <vladilie94@gmail.com>
2022-06-09 20:35:47 +0300

    Blink text.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,13 @@ const Home: NextPage = () => {
   const [height, setHeight] = useState<number>();
 
   useEffect(() => {
+    setInterval(() => {
+      const comma = document.getElementById('comma') as HTMLElement;
+      comma.style.opacity = comma.style.opacity === '0' ? '1' : '0';
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
     if (ref.current?.offsetHeight) {
       setHeight(ref.current?.offsetHeight);
     }
@@ -21,7 +28,9 @@ const Home: NextPage = () => {
 
       <div className='h-screen w-full bg-hero bg-cover bg-center bg-no-repeat px-6'>
         <nav ref={ref} className='container mx-auto flex justify-between py-4'>
-          <h2 className='text-white'>7:30</h2>
+          <h2 className='text-white'>
+            7<span id='comma'>:</span>30
+          </h2>
         </nav>
 
         <main
